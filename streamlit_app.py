168826f4e455db2140f13c5306f12ca8e98ae75d
anandmenonjobs <117369826+anandmenonjobs@users.noreply.github.com>
2023-05-11 16:06:46 +0530

    Update streamlit_app.py
    closing connection and inserting correct value

--- streamlit_app.py
+++ streamlit_app.py
@@ -60,17 +60,19 @@ def get_fruit_load_list():
 if streamlit.button('Get Fruit Load list'):
    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
    my_data_rows = get_fruit_load_list()
+   my_cnx.close()
    streamlit.dataframe(my_data_rows)
 
 #streamlit.stop()
 def insert_row_snowflake(fruit_choice):
    with my_cnx.cursor() as my_cur:
-      my_cur.execute("insert into fruit_load_list values('from streamlit')")
+      my_cur.execute("insert into fruit_load_list values(fruit_choice)")
       return 'Thanks for adding ' + fruit_choice
 fruit_choice_second = streamlit.text_input('What fruit would you like to add?','Jackfruit')
 if streamlit.button('Add fruit to list'):
     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
     row_insert = insert_row_snowflake(fruit_choice_second)
+    my_cnx.close()
     streamlit.text(row_insert)
     
     
